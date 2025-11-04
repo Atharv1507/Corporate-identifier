b7e18082a073ed2c2b2fb8f70086c26d752d4660
Atharv Aggarwal <atharv@Atharvs-MacBook-Air.local>
2025-11-05 01:02:21 +0530

    Added the main template of ui

--- Corporater/src/Header.jsx
+++ Corporater/src/Header.jsx
@@ -1,4 +1,15 @@
-function Header(){
+import './header.css'
 
+function Header(){
+    return (
+        <>
+        <div className="header">
+            <div className="title">
+            <h1>Corporate Crackker</h1>
+            </div>
+          <h2>Debunkify the Corporate Lingo!</h2>
+        </div>
+        </>
+    )
 }
 export default Header;
