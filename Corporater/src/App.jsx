d018004af34dee8716e2c5595c61c4ad441e12b0
Atharv Aggarwal <atharv@Atharvs-MacBook-Air.local>
2025-11-05 13:18:32 +0530

    Basic ui implemented

--- Corporater/src/App.jsx
+++ Corporater/src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Header"
 import Footer from "./Footer"
-
+import Score from "./Score"
+import DecodeBox from "./DecodeBox"
 function App() {
 
 
   return (
     <>
      <Header></Header>
+     <DecodeBox></DecodeBox>
+      <Score></Score>
      <Footer/>
     </>
   )
